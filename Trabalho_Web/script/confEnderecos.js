a4a34e1157df2b70430e824a3496029dbe0f1897
Fabinho <fhenriqueluzsantos@gmail.com>
2024-10-27 18:44:38 -0300

    começo da atualização de endereços

--- Trabalho_Web/script/confEnderecos.js
+++ Trabalho_Web/script/confEnderecos.js
@@ -1,5 +1,6 @@
-
+let lastID;
 async function listarendereco(){
+
 
   let resposta = await fetch(url,{
     method: "GET",
@@ -32,9 +33,8 @@ async function listarendereco(){
     console.log(id,title,cep,address,numero,complemento)
     const tabelaBody = document.getElementById("minhaTabela");
     const novaLinha = tabelaBody.insertRow();
-    novaLinha.innerHTML = `<td>${title}</td><td>${cep}</td><td>${address}</td><td>${numero}</td><td>${complemento}</td> <button><a href="../index/atualize_endereco.html">atualizar</a></button>`;
+    novaLinha.innerHTML = `<td>${title}</td><td>${cep}</td><td>${address}</td><td>${numero}</td><td>${complemento}</td> <button><a href="../index/atualize_endereco.html?id=${id}">atualizar</a></button>`;
 
-  
   });
  }
  let chamar = listar();
@@ -43,8 +43,13 @@ async function listarendereco(){
 }
 
  async function atualizarEndereco(){
-   let id = 1730;
-   let urlID ="https://go-wash-api.onrender.com/api/auth/address/"+id
+   
+  
+  
+  
+  let id = 1682
+   
+   let urlID = "https://go-wash-api.onrender.com/api/auth/address/" + id
 
    let resposta = await fetch(urlID,{
     method: "GET",
@@ -54,8 +59,10 @@ async function listarendereco(){
     }
 
   });
+  
+  
   if(resposta.ok){
-    const teste = await resposta.json();
+    let teste = await resposta.json();
     console.log(teste.data);
     listar(teste.data)
 
@@ -66,19 +73,18 @@ async function listarendereco(){
 
   
  async function listar(teste){
- let end = await teste.forEach(endereco => {
-   
-    let id = endereco.id
-    let title = endereco.title
-    let cep = endereco.cep 
-    let address = endereco.address
-    let numero = endereco.number 
-    let complemento = endereco.complement
-    console.log(id,title,cep,address,numero,complemento)
+  
+  let title = document.getElementById("title");
+  let cep = document.getElementById("cep")
+  let address = document.getElementById("address");
+  let numero = document.getElementById("numero");
+  let complemento = document.getElementById("complement");
+      
+  
     
 
   
-  });
+  
 
 }
  
